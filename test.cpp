600c097921ae1103a218cedd3dd8e3a0aee03f8d
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-04-05 13:41:06 -0230

    Finally not lost in the sauce

--- test.cpp
+++ test.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
+int myNum = 15, y = 5, z = 6;
+string txt = "ASASAS";
+const int x  = 11;
+
+string name;
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!", "and from Visual Studio"};
+    cout << myNum<<y<<z<<endl;
+    cout << x<<endl;
+    cout<<txt.length()<<endl;
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    // getline(cin, name);
+    // cout << name;
+
+    int myVar = 0;
+    while (myVar< 5) {
+        cout << myVar;
+        myVar++;
     }
-    cout << endl;
+    return 0;
 }
+// open git bash to compile then g++ test.cpp -o test
